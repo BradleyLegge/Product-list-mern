80569c6011620a7990254d2f98c548480d4338f3
BradleyLegge <bmlegge12@gmail.com>
2025-06-05 15:21:39 -0400

    added frontend

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <h1>Brads Dessert Diner</h1>
+      <nav>
+        <ul>
+          <li>Products</li>
+          <li>About</li>
+          <li>Log-in</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
